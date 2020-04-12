09415cefb1f869df04167c810cfcb41959161632
Riku <joker_avan@icloud.com>
2020-04-12 13:34:33 +0900

    rails test できるようにした deviseを用いてviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#home'
-  get 'index', to: 'users#index'
+  root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,17 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def log_in(user)
+    if integration_test?
+      #use warden helper
+      login_as(user, :scope => :user)
+    else
+      #use devise helper
+      sign_in(user)
+    end
+  end
+
+  def integration_test?
+    defined? (post_via_redirect)
+  end
 end
