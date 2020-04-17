801a80870c88cf6be5ff76253af603c1693e90d4
Riku <joker_avan@icloud.com>
2020-04-17 11:14:11 +0900

    何かしらのエラーが発生したときのhtmlと全体に適用するフォームとarticles/newのviewを作成 _form_html.erbは要修正、createが作成されていないため記事ができない状態

--- test/controllers/article_controller_test.rb
+++ test/controllers/article_controller_test.rb
@@ -1,11 +1,20 @@
 require 'test_helper'
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class ArticlesControllerTest <  ActionController::TestCase
+  # include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
+
 
   def setup
-    @article = Article.create(title: "apple")
+    @article = Article.create(title: "apple", content: "apple is red")
+    @user = users(:riku)
+    sign_in(@user)
   end
 
-  test 'article'
+  test 'should get new' do
+    get new_article_path
+    assert_response :success
+
+  end
 
 end
