ad6ba542f996900345dd0fe0efaf25389e239720
Riku <joker_avan@icloud.com>
2020-04-19 18:09:12 +0900

    Log inページの完成、後はテキスト,ロゴなどの調整, test,_copy.html.erbはただのコピペ用で意味はなし 最後に削除予定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_save {self.email = email.downcase}
+  has_many :articles
 end
