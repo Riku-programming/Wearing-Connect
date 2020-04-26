438c206bd0e41913fac5a485d20d55d7926605ee
Riku <joker_avan@icloud.com>
2020-04-27 00:11:18 +0900

    JavaScriptを用いたAjax機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,23 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friends = params[:friend]
+      if @friends
+        respond_to do |format|
+          format.js {render partial: 'users/friend_result'}
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js {render partial: 'users/friend_result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js {render partial: 'users/friend_result'}
+      end
+    end
   end
-
 end
