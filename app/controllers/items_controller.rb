95ffc1d813d910f4f62a382b21c0e47b4c134a5d
Riku <joker_avan@icloud.com>
2020-05-08 16:46:18 +0900

    itemを検索した結果のlistをajax化,不具合修正 アイテムが見つからなかった時、何も入力せず検索した時のエラーメッセージを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,27 @@
 class ItemsController < ApplicationController
+
+  def my_items
+  end
+
+
   def search
     if params[:keyword].present?
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
-      if @items.present?
+      if @items.count >= 1
         respond_to do |format|
-          format.js {render partial: 'item_result.js.erb'}
+          format.js {render partial: 'item_result'}
         end
-      else
+      elsif @items.count == 0
         respond_to do |format|
           flash.now[:alert] = "Couldn't find item"
-          format.js {render partial: 'item_result.js.erb'}
+          format.js {render partial: 'item_result'}
         end
       end
-      # fixme itemを入力しなかった時messageを出すようにしたい
-    # else
-    #   respond_to do |format|
-    #     flash.now[:alert] = "Please enter a item name to search"
-    #     format.js {render partial: 'item_result.js.erb'}
-    #   end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a item name to search"
+        format.js {render partial: 'item_result'}
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'search_item', to: 'items#search'
-  # get 'my_items', to: 'items#my_items'
+  get 'my_items', to: 'items#my_items'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
