28d59dd336473fa99f7de2269fa27a6565357278
Riku <joker_avan@icloud.com>
2020-05-08 14:53:45 +0900

    itemを検索した結果のlistをajax化した、ただまだ不具合があるので要修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
   def search
-    if params[:keyword]
+    if params[:keyword].present?
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      if @items.present?
+        respond_to do |format|
+          format.js {render partial: 'item_result.js.erb'}
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find item"
+          format.js {render partial: 'item_result.js.erb'}
+        end
+      end
+      # fixme itemを入力しなかった時messageを出すようにしたい
+    # else
+    #   respond_to do |format|
+    #     flash.now[:alert] = "Please enter a item name to search"
+    #     format.js {render partial: 'item_result.js.erb'}
+    #   end
     end
   end
 end
