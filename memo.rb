a1eb0326a91c371d5d3a32675739293c532c9aec
Riku <joker_avan@icloud.com>
2020-05-09 16:06:41 +0900

    itemを検索した結果のlistをajax化,不具合修正 アイテムが見つからなかった時、何も入力せず検索した時のエラーメッセージを追加

--- memo.rb
+++ memo.rb
@@ -1,15 +1,20 @@
 # 今からやること
-# todo paginationつける
-# todo 検索結果をajax化する
+# todo お気に入り機能つける(自分のページにお気に入り登録できるようにする)
+# todo おすすめ機能(要検討)つける この商品をお気に入り登録しているユーザを表示的な
+# todo itemとarticleをidか何かで紐付け(belong_toとか)
 
-
-
+# UI UXの修正案
+# todo ユーザーのアイコンを一番右にする
+# todo アイコンをクリックしたら自分のpageにいくように
+# todo タイムライン的な
 
 
 
 
 # 後ほど修正
+# todo testを追加(RSpec)
 # todo flash messageを修正
+# todo　日本語化する
 
 
 
