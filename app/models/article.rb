32230061144ac1de7f2421f52a816eeedbdb5797
Riku <joker_avan@icloud.com>
2020-05-31 19:42:03 +0900

    Article Controllerのテスト、createアクションのところに不具合あり

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :content, presence: true, length: { minimum: 10, maximum: 300 }
 
+
   def favorited_by?(user_id)
     favorites.find_by(user_id: user_id)
   end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,22 +1,67 @@
 require 'rails_helper'
 
 # Change this ArticlesController to your project
-RSpec.describe ArticlesController, type: :controller do
-  login_user
-  # This should return the minimal set of attributes required to create a valid
-  # Article. As you add validations to Article, be sure to adjust the attributes here as well.
-  let(:valid_attributes) {
-    { :title => "Test title!", :content => "This is a test description" }
-  }
+RSpec.describe "Articles", type: :request do
+  let!('user') {FactoryBot.create(:user)}
+  let!('another_user') {FactoryBot.create(:another_user)}
+  let!('item') {FactoryBot.create(:item)}
+  let!('another_item') {FactoryBot.create(:another_item)}
+  # user_id 1 item_id 1
+  let('article') {FactoryBot.create(:article)}
+  let('article_params') {FactoryBot.attributes_for(:article)}
+  # user_id 1 Item_id 2
+  let('another_article') {FactoryBot.create(:another_item_article)}
+  let('another_article_params') {FactoryBot.attributes_for(:another_item_article)}
+  # user_id 2 item_id 1
+  let('another_user_article') {FactoryBot.create(:another_user_article)}
+  let('another_user_article_params') {FactoryBot.attributes_for(:another_user_article)}
+  # user_id 2 item_id 2
+  let('all_other_article') {FactoryBot.create(:all_other_article)}
+  let('all_other_article_params') {FactoryBot.attributes_for(:all_other_article)}
 
-  let(:valid_session) { {} }
+  describe '#index' do
+    context 'ログインしていない状態の場合' do
+      it 'ログインページにリダイレクトされること' do
+        get item_articles_path(item)
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to '/users/sign_in'
+      end
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Article.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful # be_successful expects a HTTP Status code of 200
-      # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
     end
+
+  end
+
+
+  describe '#new' do
+    context 'ログインしていない状態の場合' do
+      it 'ログインページにリダイレクトされること' do
+        get new_item_article_path(item)
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+
+    context 'ログインしている状態の場合' do
+      it 'successステータスを返すこと' do
+        get new_item_article_path(item)
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+
+  # fixme createのテストが書けない
+  describe '#create' do
+    context 'ログインしている状態の場合' do
+      before do
+        sign_in user
+      end
+      it '記事を作成すること' do
+        expect{post item_articles_path(item),
+                    params: {article: article_params}}.to change{Article.count}.by(1)
+      end
+    end
+
   end
 end
