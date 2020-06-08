bd87bf7768b58689e8f1f795004eb97f79186dba
Riku <joker_avan@icloud.com>
2020-06-08 17:42:17 +0900

    headerのViewを整えた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,15 @@ module ApplicationHelper
     current_user && warden.session(:user)
   end
 
+  def header_link_item(name, path)
+    class_name = 'nav-item'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class: class_name do
+      link_to name, path, class: 'nav-link'
+    end
+  end
+
 
 
 end
