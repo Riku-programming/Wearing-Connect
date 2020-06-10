094624c51347a7937a874f0b8b4b1e4fb70cb2e9
Riku <joker_avan@icloud.com>
2020-06-10 14:07:38 +0900

    NofileのMigration Fileの削除,新しいMigration Fileの作成

---
+++ db/migrate/20200609083624_remove_url_from_items.rb
@@ -0,0 +1,6 @@
+class RemoveUrlFromItems < ActiveRecord::Migration[6.0]
+  def change
+
+    remove_column :items, :url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_090500) do
+ActiveRecord::Schema.define(version: 2020_06_09_083932) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,10 +40,8 @@ ActiveRecord::Schema.define(version: 2020_06_07_090500) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer "user_id"
     t.string "item_name"
     t.integer "price"
-    t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
