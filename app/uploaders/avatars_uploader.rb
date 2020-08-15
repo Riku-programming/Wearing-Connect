63b0d1ce3f2a3b24a83ee4dcbc2f47e197afcda2
Riku <joker_avan@icloud.com>
2020-08-15 11:53:40 +0900

    s3を使わず投稿することに

--- app/uploaders/avatars_uploader.rb
+++ app/uploaders/avatars_uploader.rb
@@ -1,18 +1,19 @@
 class AvatarsUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
-  #
   include CarrierWave::MiniMagick
 
-  if Rails.env.production? || Rails.env.staging?
-    storage :fog
-  else
-    storage :file
-  end
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+  #
+
+
+  # if Rails.env.production? || Rails.env.staging?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
